7542eb3ca5b607dc2a1b5c47288b182a4aa0104b
Astral <132731805+astralsc@users.noreply.github.com>
2026-05-03 20:08:00 +0800

    safety needed

--- 404/index.php
+++ 404/index.php
@@ -5,7 +5,7 @@ $url = $_SERVER['REQUEST_URI'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $time = date('Y-m-d H:i:s');
 $method = $_SERVER['REQUEST_METHOD'];
-$entry = "[$time] 404 - $method $url - IP: $ip\n";
+$entry = "[$time] 404 - $method $url\n";
 file_put_contents($logFile, $entry, FILE_APPEND);
 
 http_response_code(404);
