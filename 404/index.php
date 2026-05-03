152142c2fa1d81a16622af54e1044110657538de
Astral <132731805+astralsc@users.noreply.github.com>
2026-05-03 20:06:31 +0800

    logger

---
+++ 404/index.php
@@ -0,0 +1,14 @@
+<?php
+$logFile = __DIR__ . '/404_log.txt';
+
+$url = $_SERVER['REQUEST_URI'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$time = date('Y-m-d H:i:s');
+$method = $_SERVER['REQUEST_METHOD'];
+$entry = "[$time] 404 - $method $url - IP: $ip\n";
+file_put_contents($logFile, $entry, FILE_APPEND);
+
+http_response_code(404);
+
+echo "ok u hit 404";
+?>
